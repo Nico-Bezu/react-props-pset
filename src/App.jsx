5b682e633af61091cb645e9a59b1e6737491fab5
Nicodemus <nbezuayeho@gmail.com>
2024-11-13 19:48:55 -0500

    comp 1 finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import CustomButton from './CustomButton';
+import RandomCard from './RandomCard';
 
 function App() {
   return (
     <>
       <h1>Custom Components Practice</h1>
-      <CustomButton text="Click Me" bgcolor="cornflowerblue" alertMessage="you clicked me!" />
+      <CustomButton text="Click Me" bgcolor="cornflowerblue" alertMessage= "the big 10!" />
       {/* Add your custom components here */}
+      <RandomCard value={6} backgroundColor={"brown"} />
     </>
   );
 }
--- src/RandomCard.jsx
+++ src/RandomCard.jsx
@@ -1,14 +1,25 @@
 import './RandomCard.css';
 
-function RandomCard({description, backgroundColor }) {
+function RandomCard({value, backgroundColor }) {
 
-  const value = Math.floor(Math.random() * 13) + 1;
+  var description
+  if (value < 10) {
+    description = "small card"
+  }
+  else if (value >= 10 && value < 100) {
+    description = "not too bad"
+  }
+  else {
+    description = "insane card"
+  }
+ 
   const handleClick = () => {
-    alert(alertMessage);
+    alert("This number is " + description);
   };
 
   return (
     <button className="random-card" style={{ backgroundColor: bgcolor }} onClick={handleClick}>
+          <div className="card-value">{value}</div>
           <div className="card-description">{description}</div>
     </button>
   );
